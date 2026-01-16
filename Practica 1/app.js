3f1c8b629529cc2b6a926acba47c59c3e747e0ee
Gabriel Gámez <99066680+GabriielGamez@users.noreply.github.com>
2026-01-15 20:59:48 -0600

    add:files
    Agregué los archivos de mi primera practica

---
+++ Practica 1/app.js
@@ -0,0 +1,27 @@
+const baseDeDatosCloud = [
+    { nombre: "Amazon EC2", tipo: "IaaS", estado: "Activo", costo: 35.00 },
+    { nombre: "Google Drive Enterprise", tipo: "SaaS", estado: "Activo", costo: 12.50 },
+    { nombre: "Heroku App Server", tipo: "PaaS", estado: "Inactivo", costo: 0.00 },
+    { nombre: "Azure Virtual Machines", tipo: "IaaS", estado: "Activo", costo: 40.00 }
+]
+
+const cargarServicios=()=>{
+    const contenedor = document.getElementById("contenedor-servicios")
+    contenedor.innerHTML=""
+
+    baseDeDatosCloud.forEach((servicio)=>{
+        let claseEstado;
+        if(servicio.estado == "Activo"){
+            claseEstado = "activo"
+        }
+        else{
+            claseEstado = "inactivo"
+        }
+        const tarjeta = `<div class='card'><h3>${servicio.nombre}</h3> <p class="tipo">${servicio.tipo}</p>
+                <p class="${claseEstado}">${servicio.estado}</p>
+                <p>Costo: $${servicio.costo}</p>
+            </div>
+        `;
+        contenedor.innerHTML+=tarjeta;
+    })
+}
